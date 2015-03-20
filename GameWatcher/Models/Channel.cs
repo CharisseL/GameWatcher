66022e7918b20798253e2cae86e82e789935cb52
CharisseL <charisse8@gmail.com>
2015-03-20 09:56:54 -0500

    DbSet (Games, Channels tables)

--- GameWatcher/Models/Channel.cs
+++ GameWatcher/Models/Channel.cs
@@ -2,11 +2,13 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameWatcher.Models
 {
-    class Channel
+    public class Channel
     {
+        [Key]
         public int ChannelId { get; set; }
         public string ChannelName { get; set; }
     }
--- GameWatcher/Models/Game.cs
+++ GameWatcher/Models/Game.cs
@@ -2,11 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations;
 
 namespace GameWatcher.Models
 {
     public class Game
     {
+        [Key]
         public int GameId { get; set; }
         public string AwayTeam { get; set; }
         public string HomeTeam { get; set;}
