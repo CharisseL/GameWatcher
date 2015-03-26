5953d509763af050741a031a6fd35a099a20f464
CharisseL <charisse8@gmail.com>
2015-03-26 11:00:02 -0500

    Ladda load;  button attempt (wip)

--- GameWatcher/app/Angular/main.js
+++ GameWatcher/app/Angular/main.js
@@ -1,4 +1,16 @@
 ﻿; (function () {
     'use strict';
     angular.module('watcherApp', ['ngRoute'])
+    .directive('loadingButton', function () {
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                $(element).ladda();
+
+                $(element).ladda(start);
+
+            }
+        };
+
+    });
 })();
